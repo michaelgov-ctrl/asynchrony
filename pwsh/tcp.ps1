ece9bc698b5a9fad51ac258e5fa816ef9a877061
Michael Governanti <mike@chocolatey.io>
2025-11-06 13:51:58 -0500

    cleanup

--- pwsh/tcp.ps1
+++ pwsh/tcp.ps1
@@ -9,26 +9,26 @@ class Listener {
         $this.L.Start()
     }
 
-[void]Accept() {
-    $msg = [System.Text.Encoding]::ASCII.GetBytes("you connected to port {0}!" -f $this.Port)
-    $end = (Get-Date).AddMinutes(1)
+    [void]Accept() {
+        $msg = [System.Text.Encoding]::ASCII.GetBytes("you connected to port {0}!" -f $this.Port)
+        $end = (Get-Date).AddMinutes(1)
 
-    while ((Get-Date) -lt $end) {
-        if (-not $this.L.Pending()) {
-            Start-Sleep -Milliseconds 200
-            continue
+        while ((Get-Date) -lt $end) {
+            if (-not $this.L.Pending()) {
+                Start-Sleep -Milliseconds 200
+                continue
+            }
+
+            $c = $this.L.AcceptTcpClient()
+            $conn = $c.GetStream()
+            $conn.Write($msg, 0, $msg.Length)
+            $conn.Flush()
+                    
+            $conn.Dispose()
         }
 
-        $c = $this.L.AcceptTcpClient()
-        $conn = $c.GetStream()
-        $conn.Write($msg, 0, $msg.Length)
-        $conn.Flush()
-                
-        $conn.Dispose()
+        $this.Close()
     }
-
-    $this.Close()
-}
 
     [void]Close() {
         $this.L.Close()
@@ -51,6 +51,7 @@ class Client {
         $buf = New-Object byte[] 1024
         $len = $conn.Read($buf, 0, $buf.Length)
         Write-Host ("connected with handshake: {0}" -f [System.Text.Encoding]::ASCII.GetString($buf, 0, $len))
+        $conn.Dispose()
         $this.Close()
     }
 
